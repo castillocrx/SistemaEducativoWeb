7d508534116a595e424f5ff6eab97712bd9fca10
BryanMJ04 <109931931+BryanMJ04@users.noreply.github.com>
2024-11-27 22:26:15 -0600

    CRUDS

---
+++ Controllers/CursosController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaEducativoWeb.Models;
+
+namespace SistemaEducativoWeb.Controllers
+{
+    public class CursosController : Controller
+    {
+        private readonly SistemaEducativoWebContext _context;
+
+        public CursosController(SistemaEducativoWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cursos
+        public async Task<IActionResult> Index()
+        {
+            var sistemaEducativoWebContext = _context.Curso.Include(c => c.Programa).Include(c => c.Tutor);
+            return View(await sistemaEducativoWebContext.ToListAsync());
+        }
+
+        // GET: Cursos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Curso
+                .Include(c => c.Programa)
+                .Include(c => c.Tutor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+
+            return View(curso);
+        }
+
+        // GET: Cursos/Create
+        public IActionResult Create()
+        {
+            ViewData["ProgramaId"] = new SelectList(_context.Programa, "Id", "Nombre");
+            ViewData["TutorId"] = new SelectList(_context.Tutor, "Id", "Apellidos");
+            return View();
+        }
+
+        // POST: Cursos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Descripcion,Duracion,TutorId,ProgramaId")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(curso);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProgramaId"] = new SelectList(_context.Programa, "Id", "Nombre", curso.ProgramaId);
+            ViewData["TutorId"] = new SelectList(_context.Tutor, "Id", "Apellidos", curso.TutorId);
+            return View(curso);
+        }
+
+        // GET: Cursos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Curso.FindAsync(id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProgramaId"] = new SelectList(_context.Programa, "Id", "Nombre", curso.ProgramaId);
+            ViewData["TutorId"] = new SelectList(_context.Tutor, "Id", "Apellidos", curso.TutorId);
+            return View(curso);
+        }
+
+        // POST: Cursos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Descripcion,Duracion,TutorId,ProgramaId")] Curso curso)
+        {
+            if (id != curso.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(curso);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CursoExists(curso.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProgramaId"] = new SelectList(_context.Programa, "Id", "Nombre", curso.ProgramaId);
+            ViewData["TutorId"] = new SelectList(_context.Tutor, "Id", "Apellidos", curso.TutorId);
+            return View(curso);
+        }
+
+        // GET: Cursos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Curso
+                .Include(c => c.Programa)
+                .Include(c => c.Tutor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+
+            return View(curso);
+        }
+
+        // POST: Cursos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var curso = await _context.Curso.FindAsync(id);
+            if (curso != null)
+            {
+                _context.Curso.Remove(curso);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CursoExists(int id)
+        {
+            return _context.Curso.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/MatriculasController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaEducativoWeb.Models;
+
+namespace SistemaEducativoWeb.Controllers
+{
+    public class MatriculasController : Controller
+    {
+        private readonly SistemaEducativoWebContext _context;
+
+        public MatriculasController(SistemaEducativoWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Matriculas
+        public async Task<IActionResult> Index()
+        {
+            var sistemaEducativoWebContext = _context.Matricula.Include(m => m.Curso).Include(m => m.Estudiante);
+            return View(await sistemaEducativoWebContext.ToListAsync());
+        }
+
+        // GET: Matriculas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var matricula = await _context.Matricula
+                .Include(m => m.Curso)
+                .Include(m => m.Estudiante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (matricula == null)
+            {
+                return NotFound();
+            }
+
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Create
+        public IActionResult Create()
+        {
+            ViewData["CursoId"] = new SelectList(_context.Curso, "Id", "Nombre");
+            ViewData["EstudianteId"] = new SelectList(_context.Estudiante, "Id", "Apellidos");
+            return View();
+        }
+
+        // POST: Matriculas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,EstudianteId,CursoId,Estado,FechaInscripcion")] Matricula matricula)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(matricula);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CursoId"] = new SelectList(_context.Curso, "Id", "Nombre", matricula.CursoId);
+            ViewData["EstudianteId"] = new SelectList(_context.Estudiante, "Id", "Apellidos", matricula.EstudianteId);
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var matricula = await _context.Matricula.FindAsync(id);
+            if (matricula == null)
+            {
+                return NotFound();
+            }
+            ViewData["CursoId"] = new SelectList(_context.Curso, "Id", "Nombre", matricula.CursoId);
+            ViewData["EstudianteId"] = new SelectList(_context.Estudiante, "Id", "Apellidos", matricula.EstudianteId);
+            return View(matricula);
+        }
+
+        // POST: Matriculas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,EstudianteId,CursoId,Estado,FechaInscripcion")] Matricula matricula)
+        {
+            if (id != matricula.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(matricula);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MatriculaExists(matricula.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CursoId"] = new SelectList(_context.Curso, "Id", "Nombre", matricula.CursoId);
+            ViewData["EstudianteId"] = new SelectList(_context.Estudiante, "Id", "Apellidos", matricula.EstudianteId);
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var matricula = await _context.Matricula
+                .Include(m => m.Curso)
+                .Include(m => m.Estudiante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (matricula == null)
+            {
+                return NotFound();
+            }
+
+            return View(matricula);
+        }
+
+        // POST: Matriculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var matricula = await _context.Matricula.FindAsync(id);
+            if (matricula != null)
+            {
+                _context.Matricula.Remove(matricula);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MatriculaExists(int id)
+        {
+            return _context.Matricula.Any(e => e.Id == id);
+        }
+    }
+}
